31f1c49029b4f54bdd1768f2c006557919b76626
ketu <ketu.lai@gmail.com>
2014-03-22 17:58:46 +0800

    aliexpress

--- aliexpress/forms.py
+++ aliexpress/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-
-
 class AuthForm(forms.Form):
     account = forms.EmailField(required=True)
     password = forms.CharField(required=True,widget=forms.PasswordInput)
